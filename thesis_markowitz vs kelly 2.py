93ade924defe77fc5b4a46b634fd986b4f5df885
lorenzosalvi92 <57567372+lorenzosalvi92@users.noreply.github.com>
2020-11-19 21:10:22 +0100

    Add files via upload

--- thesis_markowitz vs kelly 2.py
+++ thesis_markowitz vs kelly 2.py
@@ -17,7 +17,7 @@ import matplotlib.pylab as pylab
 #################################################
 
 # x stands for the share f
-x = np.linspace(-0.2,1)
+x = np.linspace(0,1)
 
 # y stands for the long-run exponential growth rate
 y = 0.5 * np.log(1-x*0.4+(1-x)*0.01) + 0.5 * np.log(1 + x*0.5 + (1-x)*0.01)
@@ -68,7 +68,7 @@ ev_portfolio = fractions_matrix.dot(returns_matrix)
 # EV for optimum, first root and second root
 ev_optimum = optimal_fraction.dot(returns_matrix)
 ev_firstroot = first_root_fraction.dot(returns_matrix)
-ev_secondroot = second_root_fraction.dot(returns_matrix)
+#ev_secondroot = second_root_fraction.dot(returns_matrix)
 
 # STD of risk-free and risky options
 std_riskfree = 0
@@ -78,7 +78,7 @@ var_risky = (0.5*(0.5**2) + 0.5*(0.4**2) - 0.05**2)
 # STD for optimum, first root and second root
 std_optimum = (0.2**(2)*var_risky)**(1/2)
 std_firstroot = (0.575**(2)*var_risky)**(1/2)
-std_secondroot = ((-0.175)**(2)*var_risky)**(1/2)
+#std_secondroot = ((-0.175)**(2)*var_risky)**(1/2)
 
 std_portfolio = np.array(np.zeros(10))
 # STD for each value of the fraction invested in the risky asset
@@ -111,15 +111,11 @@ plt.gcf().subplots_adjust(bottom=0.25)
 x = np.linspace(0,0.6)
 ax.plot(x,0.01 + (0.05-0.01)/0.45*x,color='black',label='Capital allocation line (CAL)')
 plt.scatter(std_portfolio,ev_portfolio, s = 80, color = 'blue', label = '0<f<1') 
-#plt.scatter(0.5,0.0544, s = 80, color = 'green', label = 'w_1>1 and w_1<0') 
-#plt.scatter(-0.1,0.0133, s = 80, color = 'green') 
 plt.scatter(0, 0.01, marker='o', s=80, color="red", label = 'f=0 and f=1')   
 plt.scatter(0.45, 0.05, marker='o', s=80, color="red")  
-plt.scatter(0.2, 0.01, marker='o', s=80, color="grey", label='Inefficient portfolios')
-plt.scatter(0.4,0.03, marker ='o', s=80, color="grey")
 plt.scatter([std_optimum], [ev_optimum], marker='o', s=80, color="gold", label = 'optimum')
 plt.scatter([std_firstroot], [ev_firstroot], marker='o', s=80, color="black", label = '0 growth') 
-plt.scatter([std_secondroot], [ev_secondroot], marker='o', s=80, color="black") 
+#plt.scatter([std_secondroot], [ev_secondroot], marker='o', s=80, color="black") 
 plt.legend(loc="upper left")
 
 # saving the image
